ed5eec1ae41be879510771d19cd5f7d084b6cb31
Saler C. Lourenço <saler.cez@gmail.com>
2024-05-09 18:45:59 -0300

    Cria editoralist

--- src/app/components/editoras/editoralist/editoralist.component.ts
+++ src/app/components/editoras/editoralist/editoralist.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild, inject } from '@angular/core';
 import { Editora } from '../../../modules/editora';
+import { MdbModalModule, MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+
 
 @Component({
   selector: 'app-editoralist',
   standalone: true,
-  imports: [],
+  imports: [MdbModalModule],
   templateUrl: './editoralist.component.html',
   styleUrl: './editoralist.component.scss'
 })
 export class EditoralistComponent {
   lista: Editora[] = [];
+  modalService = inject(MdbModalService); //eu conseguir abrir a modal... pelo TS
+  @ViewChild("modalDetalhe") modalDetalhe!: TemplateRef<any>; //enxergar o bloco de html da modal
+  modalRef!: MdbModalRef<any>; //conseguir fechar a modal aberta pelo TS
 
   constructor(){
     this.findAll();
